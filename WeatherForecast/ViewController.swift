be14c0bdbe52f9cb5be7e45337fed2de369e7d2b
Mauricio Fernández Mora <mauriciofer@gmail.com>
2021-06-24 15:43:08 -0600

    Adding basic login

--- WeatherForecast/ViewController.swift
+++ WeatherForecast/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let loginViewController = storyboard?.instantiateViewController(identifier: "login") as?
+            LoginViewController
+        
+        loginViewController?.modalPresentationStyle = .overCurrentContext // Usando .fullscreen hace dismiss vuelva a llamar a viewWillAppear presentando la pantalla de login de nuevo
+        self.present(loginViewController!, animated: true, completion: nil)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
